521d6801ab2be967680ad77bc9570892df31bc67
Banele Ngwane <banele.ngwane@inndx.net>
2023-02-14 16:54:38 +0200

    initial commit

---
+++ client/src/pages/index.ts
@@ -0,0 +1,21 @@
+import AgentProfile from './agentProfile'
+import Agents from './agent'
+import AllProperties from './allProperties'
+import CreateProperties from './createProperties'
+import Home from './home'
+import { Login } from './login'
+import MyProfile from './myProfile'
+import PropertyDetails from './propertyDetails'
+import EditProperty from './editProperty'
+
+export {
+    AgentProfile,
+    Agents,
+    AllProperties,
+    CreateProperties,
+    Home,
+    Login,
+    MyProfile,
+    PropertyDetails,
+    EditProperty
+}
