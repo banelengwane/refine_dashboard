7fd050c6bb02e6e5de4af548638527e87815c019
Banele Ngwane <banele.ngwane@inndx.net>
2023-02-15 15:12:27 +0200

    home page started

--- client/src/components/layout/sider/index.tsx
+++ client/src/components/layout/sider/index.tsx
@@ -251,7 +251,9 @@ export const Sider: typeof DefaultSider = ({ render }) => {
             sx={{
               justifyContent: "center",
               minWidth: 36,
-              color: "primary.contrastText",
+              color: "#808191",
+              marginLeft: '6px',
+              marginRight: '10px'
             }}
           >
             <Dashboard />
