080d0daf736d2bccd29f54e18b90a98a9e5a3d1b
Banele Ngwane <banele.ngwane@inndx.net>
2023-02-16 13:29:35 +0200

    dashboard charts added

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useList } from '@pankod/refine-core'
 import { Box, Typography, Stack } from '@pankod/refine-mui'
 import {
@@ -12,34 +11,34 @@ import {
 const home = () => {
   return (
     <Box>
-      <Typography fontSize={25} fontWeight={700} color="11142D">
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
         Dashboard
       </Typography>
 
-      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4} >
         <PieChart 
           title="Properties for Sale"
           value={684}
           series={[75, 25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Properties for Rent"
           value={550}
           series={[60, 40]}
-          colors={['#475ae8', '#e4b8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Total Customers"
           value={5684}
           series={[75, 25]}
-          colors={['#435be8', '#e3e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Total Cities"
           value={550}
           series={[75, 25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
       </Box>
     </Box>
